456fbea77d4c3b1c4a9a7b56578152ee18ffc69e
@Shahnoor-26 <shahnoorkhan2636@gmail.com>
2025-06-28 17:23:14 +0530

    env added

---
+++ src/env/env.js
@@ -0,0 +1,7 @@
+export const env = {
+  appwrite_endpoint: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
+  appwrite_project_id: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  appwrite_storage_id: String(import.meta.env.VITE_APPWRITE_STORAGE_ID),
+  appwrite_database_id: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  appwrite_collection_id: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+};
