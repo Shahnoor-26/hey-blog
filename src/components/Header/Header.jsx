16e89f0dcbebe0ae9b665630a83e84364c85a568
@Shahnoor-26 <shahnoorkhan2636@gmail.com>
2025-07-14 14:34:15 +0530

    buttons added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { themeUpdate } from "../../store/themeSlice.js";
 import { Button, Logo } from "../index.js";
-import { MoonIcon, SunIcon } from "../Icons.jsx";
+import { MenuIcon, MoonIcon, SunIcon } from "../Icons.jsx";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -57,7 +57,7 @@ const Header = () => {
 
   return (
     <header className="sticky top-0 h-auto w-full font-semibold antialiased select-none">
-      <nav className="h-auto w-full p-2 bg-primary text-text-primary border-b-2 border-border flex justify-between items-center gap-2.5">
+      <nav className="h-auto w-full p-2 bg-primary text-text-primary text-sm md:text-base xl:text-lg border-b-2 border-border flex justify-between items-center gap-2.5">
         <section className="bg-black">
           <Link to={"/"} tabIndex={-1}>
             <Logo />
@@ -73,8 +73,8 @@ const Header = () => {
                       to={link.source}
                       className={({ isActive }) =>
                         `${
-                          isActive && "ring-2 ring-secondary"
-                        } px-4 py-2 bg-secondary block border-2 border-border rounded-md transition-all duration-200 ease-in-out`
+                          isActive && "ring-1 md:ring-2 ring-secondary"
+                        } px-2.5 py-1.5 bg-secondary block border md:border-2 border-border rounded md:rounded-md transition-all duration-200 ease-in-out`
                       }
                     >
                       {link.name}
@@ -86,19 +86,35 @@ const Header = () => {
               <li>
                 <Button
                   children={"Logout"}
-                  className="px-4 py-2 bg-secondary border-2 border-border rounded-md transition-all duration-200 ease-in-out focus:ring-2 focus:ring-secondary"
+                  className="px-2.5 py-1.5 bg-secondary border md:border-2 border-border rounded md:rounded-md transition-all duration-200 ease-in-out focus:ring-1 md:focus:ring-2 focus:ring-secondary"
                 />
               </li>
             )}
           </ul>
-          <Button className="px-4 py-2 bg-secondary border-2 border-border rounded-md transition-all duration-200 ease-in-out focus:ring-2 focus:ring-secondary">
-            Menu
+          <Button className="px-2.5 py-1.5 md:hidden bg-secondary border md:border-2 border-border rounded md:rounded-md transition-all duration-200 ease-in-out focus:ring-1 md:focus:ring-2 focus:ring-secondary">
+            <MenuIcon
+              className="h-5 w-5 md:h-6 md:w-6 xl:h-7 xl:w-7"
+              color="#ffffff"
+              stroke="2"
+            />
           </Button>
           <Button
-            className="px-4 py-2 bg-secondary border-2 border-border rounded-md transition-all duration-200 ease-in-out focus:ring-2 focus:ring-secondary"
+            className="px-2.5 py-1.5 bg-secondary border md:border-2 border-border rounded md:rounded-md transition-all duration-200 ease-in-out focus:ring-1 md:focus:ring-2 focus:ring-secondary"
             onClick={switchStatus}
           >
-            {themeStatus ? <MoonIcon /> : <SunIcon />}
+            {themeStatus ? (
+              <MoonIcon
+                className="h-5 w-5 md:h-6 md:w-6 xl:h-7 xl:w-7"
+                color="#ffffff"
+                stroke="2"
+              />
+            ) : (
+              <SunIcon
+                className="h-5 w-5 md:h-6 md:w-6 xl:h-7 xl:w-7"
+                color="#ffffff"
+                stroke="2"
+              />
+            )}
           </Button>
         </section>
       </nav>
--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -256,3 +256,44 @@ export const SunIcon = ({
     ></path>
   </svg>
 );
+
+export const MenuIcon = ({
+  height = "24",
+  width = "24",
+  className = "",
+  stroke = "1.5",
+  color = "#000000",
+  fill = "none",
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={width}
+    height={height}
+    color={color}
+    fill={fill}
+    className={className}
+  >
+    <path
+      d="M4 5L20 5"
+      stroke="currentColor"
+      strokeWidth={stroke}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M4 12L20 12"
+      stroke="currentColor"
+      strokeWidth={stroke}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M4 19L20 19"
+      stroke="currentColor"
+      strokeWidth={stroke}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
+);
