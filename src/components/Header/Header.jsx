4f6ff6a5383dc572db99ba5550a37044d3d82455
@Shahnoor-26 <shahnoorkhan2636@gmail.com>
2025-07-15 12:10:38 +0530

    header with hamburger added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { themeUpdate } from "../../store/themeSlice.js";
 import { Button, Logo } from "../index.js";
-import { MenuIcon, MoonIcon, SunIcon } from "../Icons.jsx";
+import { CrossIcon, MenuIcon, MoonIcon, SunIcon } from "../Icons.jsx";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -21,6 +21,20 @@ const Header = () => {
         html.setAttribute("data-status", "dark");
         localStorage.setItem("dark-status", "true");
         dispatch(themeUpdate({ themeStatus: true }));
+      }
+    } catch (error) {
+      console.log("");
+    }
+  };
+
+  const hamburger = () => {
+    try {
+      const menu = document.getElementById("menu-box");
+
+      if (menu.classList.contains("right-full")) {
+        menu.classList.replace("right-full", "right-0");
+      } else {
+        menu.classList.replace("right-0", "right-full");
       }
     } catch (error) {
       console.log("");
@@ -57,7 +71,7 @@ const Header = () => {
 
   return (
     <header className="sticky top-0 h-auto w-full font-semibold antialiased select-none">
-      <nav className="h-auto w-full p-2 bg-primary text-text-primary text-sm md:text-base xl:text-lg border-b-2 border-border flex justify-between items-center gap-2.5">
+      <nav className="relative h-auto w-full p-2 bg-primary text-text-primary text-sm md:text-base xl:text-lg border-b-2 border-border flex justify-between items-center gap-2.5">
         <section className="bg-black">
           <Link to={"/"} tabIndex={-1}>
             <Logo />
@@ -65,7 +79,7 @@ const Header = () => {
         </section>
         <section className="h-auto w-auto flex items-center gap-2.5">
           <ul className="h-auto w-auto hidden md:flex items-center gap-2.5">
-            {links.map(
+            {links?.map(
               (link) =>
                 link.active && (
                   <li key={link.name}>
@@ -91,31 +105,68 @@ const Header = () => {
               </li>
             )}
           </ul>
-          <Button className="px-2.5 py-1.5 md:hidden bg-secondary border md:border-2 border-border rounded md:rounded-md transition-all duration-200 ease-in-out focus:ring-1 md:focus:ring-2 focus:ring-secondary">
-            <MenuIcon
-              className="h-5 w-5 md:h-6 md:w-6 xl:h-7 xl:w-7"
-              color="#ffffff"
-              stroke="2"
-            />
-          </Button>
-          <Button
-            className="px-2.5 py-1.5 bg-secondary border md:border-2 border-border rounded md:rounded-md transition-all duration-200 ease-in-out focus:ring-1 md:focus:ring-2 focus:ring-secondary"
-            onClick={switchStatus}
-          >
-            {themeStatus ? (
-              <MoonIcon
+          <div className="h-auto w-auto flex items-center gap-2.5">
+            <Button
+              className="px-2.5 py-1.5 md:hidden bg-secondary border md:border-2 border-border rounded md:rounded-md transition-all duration-200 ease-in-out focus:ring-1 md:focus:ring-2 focus:ring-secondary"
+              onClick={hamburger}
+            >
+              <MenuIcon
                 className="h-5 w-5 md:h-6 md:w-6 xl:h-7 xl:w-7"
                 color="#ffffff"
                 stroke="2"
               />
-            ) : (
-              <SunIcon
-                className="h-5 w-5 md:h-6 md:w-6 xl:h-7 xl:w-7"
-                color="#ffffff"
-                stroke="2"
-              />
+            </Button>
+            <Button
+              className="px-2.5 py-1.5 bg-secondary border md:border-2 border-border rounded md:rounded-md transition-all duration-200 ease-in-out focus:ring-1 md:focus:ring-2 focus:ring-secondary"
+              onClick={switchStatus}
+            >
+              {themeStatus ? (
+                <MoonIcon
+                  className="h-5 w-5 md:h-6 md:w-6 xl:h-7 xl:w-7"
+                  color="#ffffff"
+                  stroke="2"
+                />
+              ) : (
+                <SunIcon
+                  className="h-5 w-5 md:h-6 md:w-6 xl:h-7 xl:w-7"
+                  color="#ffffff"
+                  stroke="2"
+                />
+              )}
+            </Button>
+          </div>
+        </section>
+        <section
+          id="menu-box"
+          className="absolute top-0 right-full h-screen w-full flex md:hidden justify-center items-center backdrop-blur-xs transition-all duration-200 ease-in-out bg-red-500"
+        >
+          <ul className="h-auto w-full text-xl sm:text-2xl text-center space-y-4">
+            {links?.map(
+              (link) =>
+                link.active && (
+                  <li
+                    key={link.name}
+                    className="h-auto w-4/5 sm:w-3/5 mx-auto bg-secondary border-2 border-border rounded-full transition-all duration-200 ease-in-out hover:ring-2 hover:ring-secondary"
+                  >
+                    <Link to={link.source} className="block h-full w-full p-2">
+                      {link.name}
+                    </Link>
+                  </li>
+                )
             )}
-          </Button>
+            <li>
+              <Button
+                className="h-auto w-2/5 sm:w-1/4 mx-auto p-2 bg-secondary border-2 border-border rounded-full cursor-pointer transition-all duration-200 ease-in-out hover:ring-2 hover:ring-secondary"
+                onClick={hamburger}
+              >
+                <CrossIcon
+                  className="h-7 w-7 md:h-8 md:w-8 mx-auto"
+                  color="#ffffff"
+                  stroke="2"
+                />
+              </Button>
+            </li>
+          </ul>
         </section>
       </nav>
     </header>
