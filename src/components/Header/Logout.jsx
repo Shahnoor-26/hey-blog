8b047a956ea4fb137df6353ba69c8679518f6264
@Shahnoor-26 <shahnoorkhan2636@gmail.com>
2025-08-03 17:14:45 +0530

    logout added

--- src/components/Header/Logout.jsx
+++ src/components/Header/Logout.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Auth } from "../../appwrite/authentication.js";
 import { logout as logoutStore } from "../../store/authSlice.js";
-import { Button } from "../index.js";
+import { Button, Spin } from "../index.js";
 
 const Logout = ({ children, className = "" }) => {
+  const [spin, updateSpin] = useState(false);
+
   const dispatch = useDispatch();
-  const handleLogout = () => {
+
+  const authout = () => {
+    updateSpin(true);
+
     Auth.logout()
       .then(() => dispatch(logoutStore()))
-      .catch((error) => console.log("Logout Button Error ", error));
+      .catch((error) => console.log(error))
+      .finally(() => updateSpin(false));
   };
+
   return (
-    <Button children={children} className={className} onClick={handleLogout} />
+    <Button className={className} onClick={authout}>
+      {spin && <Spin />}
+      {children}
+    </Button>
   );
 };
 
